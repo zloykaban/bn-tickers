6a9bd30ab73a0cc79f799cf41f782394978b99c9
Andrey Kononov <zloykaban@gmail.com>
2022-08-22 10:27:45 +0400

    style fixes, disabled unneede mut, disable manual parsing, added automated capitalisation for argument

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ use std::env;
 struct CurrencyPrice {
     mins: u32,
     price: String,
-
 }
 
 impl CurrencyPrice {
     async fn get(ticker: &String) -> Result<Self, ExitFailure> {
         let url = format!(
-            "https://api.binance.com/api/v3/avgPrice?symbol={}USDT", ticker
+            "https://api.binance.com/api/v3/avgPrice?symbol={}USDT",
+            ticker
         );
 
-        let url = Url::parse(&*url)?;
+        let url = Url::parse(url.as_str())?;
+
         let res = reqwest::get(url).await?.json::<CurrencyPrice>().await?;
 
         Ok(res)
@@ -25,19 +26,13 @@ impl CurrencyPrice {
 
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
-    let args: Vec<String> = env::args().collect();
-    let mut ticker: String = "BTC".to_string();
-
-    if args.len() < 2 {
-        println!("Since you didn't specify a ticker, it is defaulted to BTC.");
-    } else {
-        ticker = args[1].clone();
-    }
+    let ticker = match env::args().nth(1) {
+        Some(ticker) => ticker.to_uppercase(),
+        None => String::from("BTC"),
+    };
 
     let res = CurrencyPrice::get(&ticker).await?;
     println!("{}`s cost is {} USDT", ticker, res.price);
 
     Ok(())
-
 }
-
